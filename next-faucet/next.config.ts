f10a099503b3036de21654b3b98a74fa213290e2
lizardspace2 <pierreduponddelabrouettet@proton.me>
2026-01-10 18:14:07 +0100

    rrrr

--- next-faucet/next.config.ts
+++ next-faucet/next.config.ts
@@ -1,11 +1,7 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   serverExternalPackages: ['dilithium-crystals-js'],
-  // transpilePackages: ['dilithium-crystals-js'] // Removed due to conflict with serverExternalPackages
 };
-
 
 export default nextConfig;
